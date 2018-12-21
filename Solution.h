3b149557b42bdb282fcd9d19eec7007e0cec6cc0
zeal4u <jsz1995@live.com>
2018-12-21 18:29:54 +0800

    new problems solved

--- Solution.h
+++ Solution.h
@@ -2817,6 +2817,20 @@ public:
         if (root)
             _flatten(root);
     }
+
+    // problem 279
+    int numSquares(int n) {
+        vector<int> dp(n+1, INT32_MAX);
+        for (int i = 1; i * i <= n; i++) {
+            dp[i * i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j*j < i; j++) {
+                dp[i] = min(dp[i], 1 + dp[i - j*j]);
+            }
+        }
+        return dp[n];
+    }
 };
 
 #endif //LEETCODE_SOLUTION_H
--- TreeNode.h
+++ TreeNode.h
@@ -12,7 +12,40 @@ public:
     TreeNode* left;
     TreeNode* right;
     TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+    static TreeNode* BuildTree(vector<int> nums)
+    {
+        TreeNode *root = new TreeNode(nums[0]);
+        deque<TreeNode*> parents;
+        parents.push_back(root);
+        deque<TreeNode*> children;
+        TreeNode * parent;
+        bool left = true;
+        for (int i = 1; i < nums.size(); i++) {
+            if (left) {
+                parent = parents.front();
+                parents.pop_front();
+            }
+            if (nums[i] != INT32_MIN) {
+                TreeNode *new_node = new TreeNode(nums[i]);
+                if (left)
+                    parent->left = new_node;
+                else
+                    parent->right = new_node;
+                children.push_back(new_node);
+            }
+            left = !left;
+            if (left && parents.empty()) {
+                parents = children;
+                children.clear();
+            }
+        }
+        parent = nullptr;
+        delete parent;
+        return root;
+    }
+
 };
+
 
 
 #endif //LEETCODE_TREENODE_H
